7b8bded0f2d161d9f7031551714144c57a8b9d01
Roger Li <lakersli@gmail.com>
2017-08-15 15:54:42 -0700

    Declare associations for each model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+  belongs_to :team
+  has_one :coach, through: :team, source: 'coach'
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,10 @@
 class Team < ApplicationRecord
+  has_many :players
+  belongs_to :division
+  belongs_to :coach
+  belongs_to :owner
+
+  has_one :conference, through: :division, source: 'conference'
+
+
 end
