3f7237e16d2eab9a86a1ccb6c175f1781db5d3b8
Roger Li <email": null>
2017-08-15 20:51:14 -0700

    Write seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Conference.find_or_create_by(name: "Western")
+
+Division.find_or_create_by(name:"Pacific", conference: Conference.first)
+
+Coach.find_or_create_by(name:"Luke Walton", years_coached: 1)
+
+Owner.find_or_create_by(name:"Magic Johnson")
+
+Team.find_or_create_by(name: "Lakers", city: "Los Angeles", arena: "Staples Center", division: Division.first, coach: Coach.first, owner: Owner.first )
+
+Player.find_or_create_by(first_name: "Kobe",
+  last_name: "Bryant",
+  position: "Shooting Guard",
+  feet: 6,
+  inches: 6,
+  team: Team.first )
